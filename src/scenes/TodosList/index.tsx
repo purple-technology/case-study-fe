2a869cd166baaa2e8c5e168e8892fd6d7c73394f
eunicLab <eunicennaji@hotmail.co.uk>
2020-05-23 14:01:34 +0200

    fixed bug to display All todo items at the app's initial state instead of displaying Active todo items

--- src/scenes/TodosList/index.tsx
+++ src/scenes/TodosList/index.tsx
@@ -44,7 +44,7 @@ enum FilterEnum {
 const TodosList = () => {
 	const { t } = useTranslation()
 
-	const [filter, setFilter] = useState(FilterEnum.ACTIVE)
+	const [filter, setFilter] = useState(FilterEnum.ALL)
 	const { loading, data = { todos: [] } } = useQuery<{ todos: Todo[] }>(
 		GET_TODOS
 	)
