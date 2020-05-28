a3a3e5eb1c8fe29f6b35c56f82f70b1cea392f64
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2020-05-28 14:20:49 +0200

    fix(todolist/index.tsx): update value that is passed in switchCheck to toggle, add nbsp

--- src/scenes/TodosList/index.tsx
+++ src/scenes/TodosList/index.tsx
@@ -100,7 +100,7 @@ const TodosList = () => {
 										onChange={(e) => {
 											if (e.currentTarget.checked !== checked) {
 												switchCheck({
-													variables: { id: todos[0].id },
+													variables: { id },
 													refetchQueries: ['Todos']
 												})
 											}
@@ -108,7 +108,7 @@ const TodosList = () => {
 									/>
 									<ToggleLabel className={clsx({ checked })}>
 										<small>
-											{new Date(createdTimestamp).toLocaleDateString('en-US')}
+											{new Date(createdTimestamp).toLocaleDateString('en-US')}&nbsp;
 											{new Date(createdTimestamp).toLocaleTimeString('en-US')}
 											&nbsp;-&nbsp;
 										</small>
