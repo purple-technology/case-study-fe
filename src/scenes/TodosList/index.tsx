28f186d9a25e0d58a7cff6f43a497308e89aee45
jakubhonig <jakub@jakubhonig.com>
2020-07-15 16:43:16 +0200

    First fixes (fixed id - checkboxes working now)

--- src/scenes/TodosList/index.tsx
+++ src/scenes/TodosList/index.tsx
@@ -100,7 +100,7 @@ const TodosList = () => {
 										onChange={(e) => {
 											if (e.currentTarget.checked !== checked) {
 												switchCheck({
-													variables: { id: todos[0].id },
+													variables: { id },
 													refetchQueries: ['Todos']
 												})
 											}
@@ -109,6 +109,7 @@ const TodosList = () => {
 									<ToggleLabel className={clsx({ checked })}>
 										<small>
 											{new Date(createdTimestamp).toLocaleDateString('en-US')}
+											&nbsp; {/* added white space */}
 											{new Date(createdTimestamp).toLocaleTimeString('en-US')}
 											&nbsp;-&nbsp;
 										</small>
